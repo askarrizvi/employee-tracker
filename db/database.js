6b62294b996ed2e9627eb9ff6349063cc14cd804
Askar Rizvi <askarrizvi88@gmail.com>
2021-08-15 04:13:09 -0400

    Add department, role and employee

--- db/database.js
+++ db/database.js
@@ -30,6 +30,16 @@ class Database {
         });*/
     }
 
+    getDepartmentNames(){
+        const sql = 'SELECT name FROM department';
+        return db.promise().query(sql);
+    }
+
+    getDepartmentId(name){
+        const sql = 'SELECT id FROM department WHERE name = ?';
+        return db.promise().query(sql, name);
+    }
+
     getRoles() {
         const sql = 'SELECT * FROM role'
 
@@ -43,22 +53,37 @@ class Database {
         });*/
     }
 
-    getEmployees() {
-        const sql = 'SELECT * FROM employee'
+    getRoleId(title){
+        const sql = 'SELECT id FROM role WHERE title = ?';
+        return db.promise().query(sql, title);
+    }
 
+    getEmployees() {
+        const sql = 'SELECT * FROM employee';
         return db.promise().query(sql);
     }
 
+    getEmployeeId(firstname, lastname){
+        const sql = 'SELECT id FROM employee WHERE first_name = ? && last_name = ?'
+        const params = [firstname, lastname];
+        return db.promise().query(sql, params);
+    }
+
     addDepartment(name) {
-        return
+        const sql = 'INSERT INTO department (name) VALUES (?)';
+        return db.promise().query(sql, name);
     }
 
-    addRole() {
-        return
+    addRole(title, salary, department) {
+        const sql = 'INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)';
+        const params = [title, salary, department];
+        return db.promise().query(sql, params);
     }
 
-    addEmployee() {
-        return
+    addEmployee(firstname, lastname, role, manager) {
+        const sql = 'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)';
+        const params = [firstname, lastname, role, manager];
+        return db.promise().query(sql, params);
     }
 
     updateEmployee() {
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const inquirer = require('inquirer');
 const Database = require('./db/database');
 const cTable = require('console.table');
+const { Console } = require('console');
 
 const promptMenu = () => {
     return inquirer.prompt([
@@ -17,12 +18,100 @@ const promptMenu = () => {
 const addDepartment = () => {
     return inquirer.prompt([
         {
-            type: 'text',
+            type: 'input',
             name: 'deptname',
-            message: 'Please enter a name for the new department'
+            message: 'Please enter a name for the new department',
+            validate: (deptname) => {
+                if (deptname) {
+                    return true;
+                } else {
+                    console.log("Please enter the new department name!");
+                    return false;
+                }
+            }
         }
     ])
 };
+
+function addRole(departments){
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'rolename',
+            message: 'Please enter a name for the new role',
+            validate: (rolename) => {
+                if (rolename) {
+                    return true;
+                } else {
+                    console.log("Please enter the new role name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'rolesalary',
+            message: 'Please enter a salary for the new role',
+            validate: (rolesalary) => {
+                if (!isNaN(rolesalary)) {
+                    return true;
+                } else {
+                    console.log("Please enter a valid salary!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'list',
+            message: 'Choose the department',
+            name: 'department',
+            choices: departments,
+        }
+    ]);
+}
+
+function addEmployee(roles, managers){
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'firstname',
+            message: 'Please enter the first name for the new employee',
+            validate: (firstname) => {
+                if (firstname) {
+                    return true;
+                } else {
+                    console.log("Please enter a first name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'lastname',
+            message: 'Please enter the last name for the new employee',
+            validate: (lastname) => {
+                if (lastname) {
+                    return true;
+                } else {
+                    console.log("Please enter a last name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'list',
+            message: "Choose the employee's role",
+            name: 'role',
+            choices: roles,
+        },
+        {
+            type: 'list',
+            message: "Choose the employee's manager",
+            name: 'manager',
+            choices: managers,
+        }
+    ]);
+}
 
 function splashScreen(){
     console.log(`
@@ -62,31 +151,72 @@ function init() {
             }
             else if (choice.menu === 'Add a Department'){
                 addDepartment()
-                .then(({deptname}) => {
-
+                .then(dept => {
+                    currentDb.addDepartment(dept.deptname)
+                    .then(([rows, fields]) => {
+                        console.log(dept.deptname+" added as new Department!");
+                        init();
+                    })
                 });
-                currentDb.getEmployees()
-                .then(([rows, fields]) => {
-                    const table = cTable.getTable(rows);
-                    console.log(table);
-                    init();
-                })
             }
             else if (choice.menu === 'Add a Role'){
-                currentDb.getEmployees()
+                currentDb.getDepartmentNames()
                 .then(([rows, fields]) => {
-                    const table = cTable.getTable(rows);
-                    console.log(table);
-                    init();
-                })
+                    //var resultArray = Object.values(JSON.parse(JSON.stringify(result)))
+                    var newArr = [];
+                    rows.forEach(element => {
+                        newArr.push(element.name);
+                    });
+                    addRole(newArr)
+                    .then(role => {
+                        currentDb.getDepartmentId(role.department)
+                        .then(([rows, fields]) => {
+                            currentDb.addRole(role.rolename, role.rolesalary, rows[0].id)
+                            .then(([rows, fields]) => {
+                                console.log(role.rolename+" added as new Role!");
+                                init();
+                            });
+                        });
+                    });
+                });
             }
             else if (choice.menu === 'Add An Employee'){
-                currentDb.getEmployees()
-                .then(([rows, fields]) => {
-                    const table = cTable.getTable(rows);
-                    console.log(table);
-                    init();
-                })
+                var roleArr = [];
+                var empArr = [];
+                var roleId;
+                var manId;
+                var fn;
+                var ln;
+                currentDb.getRoles()
+                .then(([rolerows, rolefields]) => {
+                    rolerows.forEach(element => {
+                        roleArr.push(element.title);
+                    });
+                    currentDb.getEmployees()
+                    .then(([emprows, empfields]) => {
+                        emprows.forEach(element => {
+                            empArr.push(element.first_name+" "+element.last_name);
+                        });
+                        empArr.push("None");
+                        addEmployee(roleArr, empArr)
+                        .then(emp => {
+                            [fn, ln] = emp.manager.split(' ');
+                            currentDb.getRoleId(emp.role)
+                            .then(([ridrows, ridfields]) => {
+                                roleId = ridrows[0].id;
+                                currentDb.getEmployeeId(fn, ln)
+                                .then(([midrows, midfields]) => {
+                                    manId = midrows[0].id;
+                                    currentDb.addEmployee(emp.firstname, emp.lastname, roleId, manId)
+                                    .then(([rows, fields]) => {
+                                        console.log(emp.firstname+" "+emp.lastname+" added as new Role!");
+                                        init();
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
             }
             else if (choice.menu === 'Update An Employee Role'){
                 currentDb.getEmployees()
